cedfd8e8f4eb80d86741d3286f9cb6d4808a81f6
Ren <HarkaranMahiHome@gmail.com>
2025-01-13 01:30:18 -0500

    Fixed Path Error

--- tests/table_test.py
+++ tests/table_test.py
@@ -4,7 +4,7 @@ import pytest
 sys.path.insert(
     0, os.path.abspath(
         os.path.join(
-            os.path.dirname(__file__), '..', r'pytable'
+            os.path.dirname(__file__), '..', r'extdict'
         )
     )
 )
